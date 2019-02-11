8d40d08ce87bb94f1d82195dbf6b5adf0034a098
Blaney83 <professionallaney@gmail.com>
2019-02-10 19:14:02 -0700

    Initial build.

---
+++ android/app/src/main/kotlin/io/github/blaney83/namegenerator/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.github.blaney83.namegenerator
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
